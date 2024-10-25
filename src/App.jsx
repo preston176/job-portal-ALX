fc8778aae877dc926a27332a904d76ff97fe5b43
Preston M <prestonnyamweya@gmail.com>
2024-10-25 18:28:33 +0300

    Refactor routing and add navigation bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import Navbar from "./components/Navbar"
+import Jobs from "./pages/Jobs"
+import Contact from "./pages/Contact"
+import About from "./pages/About"
+import Apply from "./pages/Apply"
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        {/* Homepage Route */}
+        <Route element={<Homepage />} path="/" />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/apply/:jobId" element={<Apply />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
