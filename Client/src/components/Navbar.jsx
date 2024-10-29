c495ee3c0a1c607ecbd94b24004a0a8205426260
Preston M <prestonnyamweya@gmail.com>
2024-10-29 08:21:19 +0300

    refactor: update Navbar component to conditionally display "Get Started" or "Browse Jobs" button based on user authentication status

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 
@@ -121,12 +121,14 @@ const Navbar = () => {
                             Contact
                         </Link>
                         <Link
-                            to="/login"
+                            to={auth ? "/jobs" : "/login"}
                             className="text-gray-600 hover:text-blue-600"
                             onClick={() => setIsOpen(false)}
                         >
                             <button className="px-3 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-150">
-                                Get Started
+                            {
+                                    !auth ? "Get Started" : "Browse Jobs"
+                                }
                             </button>
                         </Link>
                     </div>
